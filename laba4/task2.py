ea87026117b6da4cda90497db2fbc8e9a339427c
arina-shalyakhina <zlata.internajty@gmail.com>
2024-10-29 19:58:41 +0300

    task2.py

--- laba4/task2.py
+++ laba4/task2.py
@@ -12,13 +12,9 @@ def task(INPUT_FILENAME, OUTPUT_FILENAME) -> None:
         reader = csv.DictReader(csvf)
         for row in reader:
             b.append(row)
-
-    #json_data = json.dumps(reader, indent=4, ensure_ascii=ensure_ascii)
     with open(OUTPUT_FILENAME, "w") as jsonf:
         st=json.dumps(b, indent=4)
         jsonf.write(st)
-
-#print(task("input.csv", "output.json"))
 if __name__ == '__main__':
     # Нужно для проверки
     task("input.csv", "output.json")
